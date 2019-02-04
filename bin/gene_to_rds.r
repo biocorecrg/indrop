ffa3fb0ff1d5e259011a9a0a92e512e70d31f703
Luca Cozzuto <lucacozzuto@gmail.com>
2019-02-04 17:58:43 +0100

    reshaping

---
+++ bin/gene_to_rds.r
@@ -0,0 +1,11 @@
+# R --slave --args infile outfile < gene_to_rds.r
+
+args<-commandArgs(TRUE)
+infile	<- args[1]
+outfile	<- args[2]
+
+if (length(args)<2) {stop("[USAGE] R --slave --args <infile> <outfile> < gene_to_rds")}
+
+a<-read.table(infile)
+b<-as.vector(a$V1)
+saveRDS(b, outfile)
